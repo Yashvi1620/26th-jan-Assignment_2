f1573d885ebe09237547b186d1a15facd70ec32c
Yashvi1620 <96307761+Yashvi1620@users.noreply.github.com>
2022-01-26 23:47:42 +0530

    Update vowel.js

--- vowel.js
+++ vowel.js
@@ -1,7 +1,7 @@
 var ch=prompt("Enter the character");
 if(ch=="a"|| ch=="A" || ch=="e" || ch=="E" || ch=="i" || ch=="I" || ch=="o" || ch=="O" || ch=="u" || ch=="U" )
 {
-alert("It is a vowel",ch);
+alert("It is a vowel");
 }
 else
 {
